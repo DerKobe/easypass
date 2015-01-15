ae49c0d608410fbf2d4713115872c361e58a5396
Philip Claren <kobe@mixxt.net>
2015-01-15 14:37:19 +0100

    initial commit

---
+++ easypass.rb
@@ -0,0 +1,10 @@
+WORDCOUNT = 4
+SEPARATOR = '-'
+
+words = []
+File.readlines('words').each { |line| words << line.gsub("\n",'') }
+
+password = []
+WORDCOUNT.times { password << words.sample }
+
+puts password.join(SEPARATOR)
